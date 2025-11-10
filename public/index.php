be2e1ffe30fa66aa460f1dc37970bd33463d74ee
Emmanuel Joshua <emiranojoshua@gmail.com>
2025-11-10 18:37:26 +0100

    implemented .env

--- public/index.php
+++ public/index.php
@@ -3,11 +3,15 @@
 declare(strict_types=1);
 
 use Core\App\App;
+use Dotenv\Dotenv;
 
 const BASE_PATH = __DIR__;
 require BASE_PATH . '/../vendor/autoload.php';
 $functions = require BASE_PATH . "/../functions.php";
 require BASE_PATH . '\..\routes\web.php';
+
+$dotenv = Dotenv::createImmutable(BASE_PATH . '/../');
+$dotenv->load();
 
 // Session::unflash();
 // App::run();
